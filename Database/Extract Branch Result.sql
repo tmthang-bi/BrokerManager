4882ad20726d6037e4a1d13edb7e4935312bf3cf
tmthang <tmthang86@live.com>
2016-02-23 09:47:08 +0700

    update

--- Database/Extract Branch Result.sql
+++ Database/Extract Branch Result.sql
@@ -1,1 +1,1 @@
-select BranchID, TradingValue, NetRevenue from BranchCalculationResult where PeriodID = '201601'
+select BranchID, TradingValue, NetRevenue from BranchCalculationResult where PeriodID = '201602'
--- Database/Extract Manager Result.sql
+++ Database/Extract Manager Result.sql
@@ -2,4 +2,4 @@ SELECT r.BrokerID as ID, b.BrokerTypeID, o.BranchID,
 b.OfficeID , 0 as groupNum, b.[Name], b2.[Name] as SupervisorName, 
  r.TradingValue, r.GrossRevenue, r.NetRevenue,ofr.TradingValue as 'OfficeTradingValue', 
 ofr.NetRevenue as 'OfficeRevenue', r.ManagementBonusRate, r.SupervisorPayment, Payment
-FROM BrokerCalculationResult r INNER JOIN Broker b ON r.BrokerID = b.ID LEFT OUTER JOIN Broker b2 on b.SupervisorBrokerID = b2.ID INNER JOIN Office o ON b.OfficeID = o.ID INNER JOIN OfficeCalculationResult ofr ON b.OfficeID = ofr.OfficeID WHERE (r.PeriodId = '201601') AND (b.BrokerTypeID = 'MAN') AND (ofr.PeriodId = '201601') ORDER BY ID
+FROM BrokerCalculationResult r INNER JOIN Broker b ON r.BrokerID = b.ID LEFT OUTER JOIN Broker b2 on b.SupervisorBrokerID = b2.ID INNER JOIN Office o ON b.OfficeID = o.ID INNER JOIN OfficeCalculationResult ofr ON b.OfficeID = ofr.OfficeID WHERE (r.PeriodId = '201602') AND (b.BrokerTypeID = 'MAN') AND (ofr.PeriodId = '201602') ORDER BY ID
